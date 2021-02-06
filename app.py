aa1188d1dab02fad7447a39b3c5d210f91c3c867
lucianodelboni <lucknfx5@gmail.com>
2021-02-06 19:52:25 -0400

    building the functionality of creating a new balance table

--- app.py
+++ app.py
@@ -1,19 +1,79 @@
 import tkinter as tk
 import requests
 import json
+import datetime
 
 form1_height = 400
 form1_width = 300
 
 root = tk.Tk()
 
-#def create_balance():
-	#adicionar funcionalidade para pedir dados em nova tela, criar e abrir este balanço.
+def create_balance():
+	root.withdraw()
 
-#def open_balance():
+	#creating a new window to enter basic parameters of the new balance
+	new_balance = tk.Toplevel()
+	var = ''
+
+	new_canvas = tk.Canvas(new_balance, height=form1_height, width=form1_width)
+	new_canvas.pack()
+
+	new_label = tk.Label(new_balance, text="Fill out the form below to create a new balance sheet:")
+	new_label.place(relx=0.02, rely=0.02)
+
+	new_frame = tk.Frame(new_balance)
+	new_frame.place(relx=0.05, rely=0.1, relwidth=0.95, relheight=0.9)
+
+	new_title_label = tk.Label(new_frame, text="Title:")
+	new_title_label.place(rely=0)
+	new_title_entry = tk.Entry(new_frame)
+	new_title_entry.place(rely=0.06, relwidth=0.95)
+
+	new_type_label = tk.Label(new_frame, text="Type:")
+	new_type_label.place(rely=0.15)
+	new_type1 = tk.Radiobutton(new_frame, text="Weekly", value="weekly", variable=str(var))
+	new_type1.place(rely=0.21)
+	new_type2 = tk.Radiobutton(new_frame, text="Bi-weekly", value="biweekly", variable=str(var))
+	new_type2.place(rely=0.26)
+	new_type3 = tk.Radiobutton(new_frame, text="Monthly", value="monthly", variable=str(var))
+	new_type3.place(rely=0.32)
+	new_type3.invoke()
+
+	new_description_label = tk.Label(new_frame, text="Description:")
+	new_description_label.place(rely=0.42)
+	new_description_entry = tk.Text(new_frame)
+	new_description_entry.place(rely=0.48, relwidth=0.95, relheight=0.25)
+
+	new_button = tk.Button(new_frame, text="Create", command= lambda: modify_json('add'))
+	new_button.place(relx= 0.4, rely=0.8)
+
+	#adicionar funcionalidade para pedir dados em nova tela, criar e abrir este balanço.
+def modify_json(action):
+	if action == "add":
+		time_stamp = datetime.datetime.now()
+		newwindow = tk.Toplevel()
+		another = tk.Canvas(newwindow, height=form1_height, width=form1_width)
+		another.pack()
+		anotherentry = tk.Label(another, text=time_stamp)
+		anotherentry.pack()
+
+
+		pass
+	elif action == "update":
+		pass
+	elif action == "delete":
+		pass
+	else:
+		pass
+
+
+def open_balance():
+	pass
 	#adicionar funcionalidade para abrir um balanço existente depois de mostrar uma lista extraída do json
 
-#def delete_balance():
+def delete_balance():
+	pass
+
 	#adicionar funcionalidade para mostrar os balanços existentes extraída do json e confirmar se quer mesmo deletar
 
 
@@ -26,13 +86,13 @@ frame.place(relx=0.2, rely=0.1, relwidth=0.8, relheight=0.8)
 label_mainpage = tk.Label(frame, text='Create, open or delete a balance below:')
 label_mainpage.place(rely=0)
 
-create_button = tk.Button(frame,text='Create', height=3, width=25)
+create_button = tk.Button(frame,text='Create', command=create_balance, height=3, width=25)
 create_button.place(rely=0.1)
 
-open_button = tk.Button(frame,text='Open', height=3, width=25)
+open_button = tk.Button(frame,text='Open', command=open_balance, height=3, width=25)
 open_button.place(rely=0.35)
 
-delete_button = tk.Button(frame,text='Delete', height=3, width=25)
+delete_button = tk.Button(frame,text='Delete', command=delete_balance, height=3, width=25)
 delete_button.place(rely=0.60)
 
 root.mainloop()
